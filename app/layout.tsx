016953f00d0d209cddfb43d17183965bb2a46972
IBIKORANEZA Dieudonne <dieudonneibikoraneza13@gmail.com>
2025-01-21 19:52:50 +0200

    Finished the dark & light mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,12 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 
 const font = Open_Sans({
   subsets: ["latin"],
-  display: "swap"
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -20,8 +22,17 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider afterSignOutUrl="/">
-      <html lang="en">
-        <body className={`${font.className}`}>{children}</body>
+      <html lang="en" suppressHydrationWarning>
+        <body className={(cn(font.className), "bg-white dark:bg-[#313338]")}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem={true}
+            storageKey="discord-theme"
+          >
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
